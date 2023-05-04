d555030b1f96c6d21f978e333c526098f85bd4ad
FahmiGhazel <107745037+FahmiGhazel@users.noreply.github.com>
2023-05-04 21:51:58 +0800

    Add files via upload

---
+++ FS102 -Final Submission/index.js
@@ -0,0 +1,73 @@
+// ------- Search Bar Animation ------- //
+
+const trigger_search = document.querySelector(".trigger_search");
+const input = document.querySelector(".input");
+
+trigger_search.addEventListener("click", () => {
+  if (!input.classList.contains("input-open")) {
+    input.classList.add("input-open");
+    trigger_search.innerHTML = "<i class='fas fa-search'></i>";
+  } else {
+    input.classList.close("input-open");
+    trigger_search.innerHTML = "<i class='fas fa-search'></i>";
+  }
+});
+// --x---- Search Bar Animation ----x-- //
+
+// ------- Search Bar Input Text ------- //
+document.getElementById("search-btn").addEventListener("click", function () {
+  const searchvalue = document.getElementById("search-box").value;
+  console.log(searchvalue);
+  if (searchvalue === "Singapore" || searchvalue === "singapore") {
+    window.location.href = "destination.html";
+  } else if (searchvalue === "Itinerary" || searchvalue === "itinerary") {
+    window.location.href = "itinerary.html";
+  } else {
+    console.log("#");
+  }
+});
+
+// --x---- Search Bar Input Text ----x-- //
+
+// ----------- Sticky Navbar ----------- //
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// ---x------- Sticky Navbar -------x--- //
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
